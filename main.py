36d599e2faf76d089d45904da1bd654ed6720c76
Aleks-zaa <144313397+Aleks-zaa@users.noreply.github.com>
2023-09-15 15:22:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,22 +6,22 @@
 # 6. Подсчёт
 # 7. Вывод результата
 
-CURRENCIES = {
-    "USD": 1,
-    "RUB": 96.25,
-    "EUR": 0.93,
-    "KZT": 466.24,
-}
+import requests
+URL = "https://api.freecurrencyapi.com/v1/latest?apikey="
+API_KEY = "fca_live_VwM4HMSqi04e36YiIDOTyNtQSxiK8NH6frVaiI9V"
+def get_actual_currencies():
+    response = requests.get(URL + API_KEY)
+    return response.json()
 
+CURRENCIES = get_actual_currencies()
 
+print(CURRENCIES)
 def convert(amount, from_currency, to_currency, currencies):
     from_value = currencies.get(from_currency)  # CURRENCIES[current_currency]
     to_value = currencies.get(to_currency)
 
     coefficient = to_value / from_value
     return round(amount * coefficient, 2)
-
-
 # 1
 print("Добро пожаловать в конвертатор валют!")
 
@@ -35,17 +35,25 @@ print("""
 
 print("Доступные валюты:")
 
-for key in CURRENCIES:
-    print(f'* {key}')
+for key in CURRENCIES['data']:
+    print(f"* {key}")
 
 # 3
-current_currency = input("Введите исходную валюту: ")
+current_currency = input("Введите исходную валюту: ").upper()
+while current_currency not in CURRENCIES['data']:
+    print('Нет такой валюты')
+    current_currency = input("Введите исходную валюту: ").upper()
 # 4
-result_currency = input("Введите результирующую валюту: ")
+result_currency = input("Введите результирующую валюту: ").upper()
+while result_currency not in CURRENCIES['data']:
+    print('Нет такой валюты')
+    result_currency = input("Введите результирующую валюту: ").upper()
+
 # 5
 amount = input("Введите количество: ")
 
+
 # 6
-result = convert(float(amount), current_currency, result_currency, CURRENCIES)
+result = convert(float(amount), current_currency, result_currency, CURRENCIES['data'])
 
 print(f'{amount} {current_currency} = {result} {result_currency}')
